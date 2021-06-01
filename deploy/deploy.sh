bf17617136d94a90ddb8173491d62480a983490c
wms2537 <swmeng@yes.my>
2021-06-01 11:32:29 +0800

    fix bugs

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-while getopts b:d: flag
-do
-    case "${flag}" in
-        b) buildNum=${OPTARG};;
-        d) dbUrl=${OPTARG};;
-    esac
-done
-
-BUILD_NUMBER=$buildNum DATABASE_URL=$dbUrl docker-compose -f /root/nodejs-jwt-auth/deploy/docker-compose.yaml up -d --build
+docker-compose up -d --build
